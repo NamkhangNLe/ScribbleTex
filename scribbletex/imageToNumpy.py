da99ed71765e50997b3b232e2c946274a12afb68
Jonathan Huang <jonathannhuang@gmail.com>
2023-11-17 23:34:31 -0500

    Got groupings done

--- scribbletex/imageToNumpy.py
+++ scribbletex/imageToNumpy.py
@@ -14,4 +14,48 @@ def toGrayscale(image):
 
     return zerosAndOnes
 
+def underlyingRows(grayImage):
+    """ The most important concept. Squashes, as of writing, all of the rows onto itself in order to see the horizontal 
+    area of each possible letter. For instance, 
+    
+    00100
+    00100    => [0, 0, 1, 0, 0]
+    00100
+    
+    """
+
+    compressed = np.any(grayImage == 1, axis=0).astype(int)
+    return compressed
+
+def underlyingCols(grayImage):
+    """ Same as above but for columns"""
+
+    compressed = np.any(grayImage == 1, axis=1).astype(int)
+    return compressed
+
+def groupOnes(rowImage):
+    """ Input a 1 x D array, create a list that has all of start and end indices of every groupings of ones in a tuple
+    """
+
+    groupings = [] # main array that will hold [begin, end] of contigous 1s 
+
+    sameString = False # state machine, baby
+    temp = []
+
+    for index in range(len(rowImage)):
+        if (rowImage[index] == 1):
+            if not sameString:
+                sameString = True
+                print(index)
+                temp.append(index)
+        else:
+            if (sameString and index != 0):
+                temp.append(index - 1)
+                groupings.append(temp)
+                temp = []
+            sameString = False
+    
+    return groupings
+            
+            
 
--- scribbletex/openCVMain.py
+++ scribbletex/openCVMain.py
@@ -1,27 +1,41 @@
-import math
 import numpy as np
 import cv2 
 
 from imageToNumpy import *
 
 def main():
-    # file paths
-    imgFileName = "ScribbleTex/scribbletex/Example.png"
+    #______________LOAD FILE_________________
+    # file paths, right now, this only runs on Jonathan's machine
+    imgFileName = "/Users/jonathan/Documents/Desktop/GT Master Files/Fall 2023/AI Hack/ScribbleTex/scribbletex/Example.png" 
 
     img = cv2.imread(imgFileName) #convert into a numpy array
     
     print("Loaded image shape: " + str(img.shape))   
+    print()
+    print()
 
+    #________________GRAYSCALE_______________________
     gray = toGrayscale(img)
     
     print("Export gray image to grayscale for testing purposes")
     # For testing purposes, export to txt
     np.savetxt("gray.txt", gray, fmt='%d')
+    print()
+    print()
+    #________________UNDERLYING ROWS AND COLS__________________
 
+    rows = underlyingRows(gray)
+    cols = underlyingCols(gray)
 
+    print("Rows: " + str(rows))
+    print("Cols: " + str(cols))
+    print()
+    print()
 
+    #_____________GROUPINGS_______________
+    testExample = np.array([0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0])
+    print("Groupings: " + str(groupOnes(rows)))
 
-    
 main()
 
 
