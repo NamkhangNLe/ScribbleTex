05083e29468bdb53ce6227f2f36f6b97d91fef14
Namkhang Le <nnguyen344@gatech.edu>
2023-11-17 21:31:27 -0500

    changed the logo location and title

--- scribbletex/src/App.js
+++ scribbletex/src/App.js
@@ -24,21 +24,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo" alt="logo" style={{ position: 'absolute', top: '0', right: '0', width: '200px' }} />
+        <p style={{ position: 'absolute', top: '0', left: '100' }}>
           ScribbleTex
         </p>
 
         <div className='spacer' />
 
-        <CanvasDraw ref = {saveableCanvas} brushRadius={1} brushColor="rgba(155,12,60,0.3)" lazyRadius="5" canvasWidth={500} canvasHeight={500} />
+        <div className="canvas-container" style={{ marginTop: '200px' }}>
+          <CanvasDraw ref={saveableCanvas} brushRadius={1} brushColor="rgba(155,12,60,0.3)" lazyRadius="5" canvasWidth={500} canvasHeight={500} />
+        </div>
           
-        <button onClick = {handleClear}>Clear</button>
-        <button onClick = {handleUpload}>Upload</button>
+        <div className="button-container">
+          <button onClick={handleClear}>Clear</button>
+          <button onClick={handleUpload}>Upload</button>
+        </div>
 
         <div className='spacer' />
 
-        <img src = {drawingDataUrl} alt = "drawing" />
+        <img src={drawingDataUrl} alt="drawing" />
 
         <a
           className="App-link"
